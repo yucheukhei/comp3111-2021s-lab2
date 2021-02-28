bcc3bb7c2688fbe83b4c022fde4e8ef8c0cfbe41
Yu Cheuk Hei Hayden <heiheiyu127@gmail.com>
2021-02-28 22:41:03 +0800

    Lab2 Assignment

---
+++ src/main/java/comp3111/lab2/ex2/Charger.java
@@ -0,0 +1,11 @@
+package comp3111.lab2.ex2;
+
+interface Chargeable {
+	public void charge();
+}
+
+public class Charger {
+	public void charge(Chargeable c) {
+		c.charge();
+	}
+}
---
+++ src/main/java/comp3111/lab2/ex2/Computer.java
@@ -0,0 +1,13 @@
+package comp3111.lab2.ex2;
+
+public class Computer {
+	protected String secret;
+	
+	public Computer() {
+		secret = "computer secret";
+	}
+	
+	public void work() {
+		System.out.println("A computer is working");
+	}
+}
